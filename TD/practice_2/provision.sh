e77b4660579e9ff37cd9656b2e4e1f79756b2765
Badre Belabbess <badre.belabbess@atos.net>
2018-03-07 14:00:05 +0100

    Converting new files into right format.

--- TD/practice_2/provision.sh
+++ TD/practice_2/provision.sh
@@ -2,6 +2,10 @@ apt-get -y update
 
 apt-get -y install nginx
 
+rm -rf /etc/nginx/sites-enabled
+
+cp -r /vagrant/sites-enabled /etc/nginx
+
 rm -rf /usr/share/nginx/www
 
 ln -s /vagrant/www/ /usr/share/nginx/www
